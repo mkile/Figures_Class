42dc2a054210d13df71acd069feb4b93e2ec7ed7
mkile <mkiles81@gmail.com>
2021-04-10 21:46:32 +0300

    First commit

--- source/classes.py
+++ source/classes.py
@@ -1,27 +1,44 @@
+from functools import reduce
+from math import pi, inf
+
+
 class Figure:
     def __init__(self, name: str, sides_list: list, sides: int):
+        if 0 in sides_list:
+            raise Exception('Создание фигур с параметром равным 0 запрещено.')
+        if self.__class__.__name__ == 'Figure':
+            raise Exception('Создание объектов класса Figure запрещено.')
+        if len(name) == 0:
+            raise Exception('Имя фигуры не может быть пустым!')
         self.name = name
         if len(sides_list) != sides:
-            raise Exception('Количество сторон фигуры не соответствует фигуре.')
+            raise Exception('Количество сторон фигуры не соответствует фигуре,' +
+                            'передано: ' + str(len(sides_list)) + ' ,а должно быть: ' + str(sides))
         self.sides_list = sides_list
         self.__sides = sides
+        if sides == 1:
+            self.angles = inf
+        else:
+            self.angles = sides
 
     def __repr__(self):
         return '<' + Figure.__name__ + '>' + \
                self.name + ':' + str(self.__sides)
 
     def area(self):
-        if len(set(self.sides_list)) == 1:
-            return self.sides_list[0] ** 2
+        sides_set = list(set(self.sides_list))
+        if len(sides_set) == 1:
+            return sides_set[0] ** 2
         else:
-            return set(self.sides_list)[0] * set(self.sides_list)[1]
+            return sides_set[0] * sides_set[1]
 
     def perimeter(self):
         return sum(self.sides_list)
 
-    def add_figure(self, other):
+    def add_area(self, other):
         if not isinstance(other, Figure):
-            raise Exception('Можно складывать только объекты класса Figure')
+            raise Exception('Метод принимает только объекты '
+                            'наследуемые от класса Figure.')
         return self.area() + other.area()
 
 
@@ -32,9 +49,46 @@ class Rectangle(Figure):
         super().__init__(name, sides_list * 2, self.sides)
 
 
-x = Rectangle('rect', [5, 5])
-y = Rectangle('rect', [5, 5])
-print(x.area())
-print(x.perimeter())
-print(x)
-print(x.add_figure(5))
+class Square(Figure):
+    sides = 4
+
+    def __init__(self, name, sides_list):
+        super().__init__(name, sides_list * 4, self.sides)
+
+
+class Triangle(Figure):
+    sides = 3
+
+    def __init__(self, name, sides_list):
+        super().__init__(name, sides_list, self.sides)
+        if not self.__check_possibility():
+            raise Exception('Треугольника с такими сторонами существовать не может')
+
+    def __check_possibility(self):
+        sides = self.sides_list + self.sides_list
+        for side_number in range(0, len(sides) - 3):
+            if sides[side_number] > (sides[side_number + 1] + sides[side_number + 2]):
+                return False
+        return True
+
+    def area(self):
+        return (self.perimeter() / 2 *
+                reduce(lambda x, y: x * y,
+                       [self.perimeter() / 2 - x for x in self.sides_list])) ** 0.5
+
+
+class Circle(Figure):
+    sides = 1
+
+    def __init__(self, name, sides_list):
+        super().__init__(name, sides_list, self.sides)
+
+    def area(self):
+        return pi * self.sides_list[0] ** 2
+
+    def perimeter(self):
+        return 2 * pi * self.sides_list[0]
+
+
+if __name__ == '__main__':
+    print('Hello')
--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -3,14 +3,25 @@ from source.classes import Triangle
 
 
 def test_creation():
-    assert Triangle('triangle', [2, 3, 5])
+    assert Triangle('triangle', [2, 4, 5])
 
 
-def test_check_name():
-    test_figure = Triangle('test_figure_name', [2, 3, 5])
-    assert test_figure == 'test_figure_name'
+def test_wrong_triangle_creation_error():
+    with pytest.raises(Exception):
+        Triangle('triangle', [2, 9, 5])
 
 
-def test_check_area():
-    test_figure = Triangle('test_figure_name', [2, 3, 5])
-    assert int(round(test_figure.area(), 0)) == 4
+def test_check_name(generic_triangle):
+    assert generic_triangle.name == 'test_figure_name'
+
+
+def test_check_area(generic_triangle):
+    assert int(round(generic_triangle.area(), 0)) == 4
+
+
+def test_check_perimeter(generic_triangle):
+    assert generic_triangle.perimeter() == 11
+
+
+def test_add_area(generic_triangle):
+    assert int(round(generic_triangle.add_area(generic_triangle), 0)) == 8
